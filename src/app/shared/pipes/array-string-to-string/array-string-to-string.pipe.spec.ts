39412e59d455497e973822d1f1effd70870890b8
Qhung <quanghung165200@gmail.com>
2023-12-02 20:49:37 +0700

    Add array-string-to-string pipe

---
+++ src/app/shared/pipes/array-string-to-string/array-string-to-string.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ArrayStringToStringPipe } from './array-string-to-string.pipe';
+
+describe('ArrayStringToStringPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ArrayStringToStringPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
