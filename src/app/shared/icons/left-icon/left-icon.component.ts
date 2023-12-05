5dfe1f324cc12bdd4c00c6ddbabcd4c7f4f4e259
Qhung <quanghung165200@gmail.com>
2023-12-05 14:48:28 +0700

    Add icons to chapter page
    Add icons: left icon and list icon

---
+++ src/app/shared/icons/left-icon/left-icon.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-left-icon',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './left-icon.component.html',
+  styleUrls: ['./left-icon.component.scss']
+})
+export class LeftIconComponent {
+
+}
---
+++ src/app/shared/icons/list-icon/list-icon.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-list-icon',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './list-icon.component.html',
+  styleUrls: ['./list-icon.component.scss']
+})
+export class ListIconComponent {
+
+}
