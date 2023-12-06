81e7270d619cce6fd968c1cbc8204ee77dbb7d0b
Qhung <quanghung165200@gmail.com>
2023-12-06 15:52:40 +0700

    Set the default 'src' attribute value in the directive
    Set the default image is the default 'src' attribute value in the image-load-failed.directive

--- src/app/shared/directives/image-load-failed/image-load-failed.directive.ts
+++ src/app/shared/directives/image-load-failed/image-load-failed.directive.ts
@@ -1,3 +1,4 @@
+import { DEFAULT_IMAGE_URL } from 'src/app/core/constants/image';
 import {
   Directive,
   ElementRef,
@@ -16,7 +17,9 @@ export class ImageLoadFailedDirective {
   @HostListener('error') handleImageLoadFailed() {
     const element: HTMLElement = this.elf.nativeElement;
     if (element.tagName === 'IMG') {
-      if (element.getAttribute('src') === this.backupSrc)
+      if (this.backupSrc === '')
+        this.renderer.setAttribute(element, 'src', DEFAULT_IMAGE_URL);
+      else if (element.getAttribute('src') === this.backupSrc)
         this.renderer.removeAttribute(element, 'src');
       else this.renderer.setAttribute(element, 'src', this.backupSrc);
     }
