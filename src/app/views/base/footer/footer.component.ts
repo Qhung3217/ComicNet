6723177808112922b81516cd0086a2acd1160817
Qhung <quanghung165200@gmail.com>
2023-12-07 14:38:01 +0700

    Add link to github

--- src/app/views/base/footer/footer.component.ts
+++ src/app/views/base/footer/footer.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { GithubIconComponent } from 'src/app/shared/icons/github-icon/github-icon.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, GithubIconComponent],
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {
-
-}
+export class FooterComponent {}
